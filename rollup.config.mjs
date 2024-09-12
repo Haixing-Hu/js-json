21124f4c3d3019e2818c80eb37ffb99bc076696d
Haixing Hu <starfish.hu@gmail.com>
2024-09-12 15:57:47 +0800

    fix: fix the filename of the building output script

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -8,4 +8,4 @@
 ////////////////////////////////////////////////////////////////////////////////
 import rollupBuilder from '@haixing_hu/rollup-builder';
 
-export default rollupBuilder('Logging', import.meta.url);
+export default rollupBuilder('Json', import.meta.url);
